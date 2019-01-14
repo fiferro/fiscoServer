0f0d71bfe86134bded6d6d0201ce0853eb769582
= <fferro@orion.tech>
2019-01-14 11:36:22 -0200

    inclusão user

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,9 +1,34 @@
-const express = require('express');
-const router = express.Router();
+
+var router = require('express').Router(),
+    bodyParser = require('body-parser'),
+    express = require("express"),
+    basicAuth = require('express-basic-auth');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+// Allow all CORS origins
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
+
+app.use(basicAuth({
+    users: { 'admin': 'fisco' }
+}))
+
 router.get('/', function (req, res, next) {
     res.status(200).send({
-        title: "Node Express API",
+        title: "Api em execução",
         version: "0.0.1"
     });
 });
+
+
+router.use('/fisco', require('./service/users'));
+
+
 module.exports = router;
