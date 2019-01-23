7487ac15c68f2fc6e0e5b0547fe684e7abec3810
= <fferro@orion.tech>
2019-01-23 09:17:17 -0200

    alteração para cadastro de empresa

--- src/routers/service/empresa.js
+++ src/routers/service/empresa.js
@@ -26,7 +26,7 @@ router.get("/empresa/:cnpj", function (req, res) {
                 res.status(500).json('unauthorized');
             }
             else {
-                var userQuery = "select * from fis_empresa where cnpj = '" + req.params.cnpj + "'";
+                var userQuery = "select * from fis_empresa where cnpj = '" + req.params.cnpj.replace('+', '/') + "'";
                 return querySql(userQuery, '', req.headers.authorization)
                     .then(function (rows) {
                         res.status(200).json({ rows });
@@ -37,8 +37,6 @@ router.get("/empresa/:cnpj", function (req, res) {
         })
 });
 
-
-
 router.get("/empresa/usuario/:id", function (req, res) {
     return db.Hash(req.headers.authorization)
         .then(function (valid) {
@@ -46,14 +44,14 @@ router.get("/empresa/usuario/:id", function (req, res) {
                 res.status(500).json('unauthorized');
             }
             else {
-                console.log(req.params.id); 
                 var userQuery = "SELECT * FROM fis_empresa_usuario feu inner join fis_empresa fe on fe.id_empresa = feu.id_empresa "
                 userQuery += "where feu.id_usuario = '" + req.params.id + "'";
                 return querySql(userQuery, '', req.headers.authorization)
                     .then(function (rows) {
                         res.status(200).json({
-                             'status': 'success',
-                            'Empr': rows });
+                            'status': 'success',
+                            'Empr': rows
+                        });
                     });
             }
 
@@ -61,9 +59,30 @@ router.get("/empresa/usuario/:id", function (req, res) {
         })
 });
 
+router.get("/empresa/usuario/minha/:id", function (req, res) {
+    return db.Hash(req.headers.authorization)
+        .then(function (valid) {
+            if (valid.length == 0) {
+                res.status(500).json('unauthorized');
+            }
+            else {
+                var userQuery = "SELECT * FROM fis_usuario_empresa fue inner join fis_usuario fu on fu.id_usuario = fue.id_usuario inner join fis_empresa fe on fe.id_empresa = fue.id_empresa "
+                userQuery += "where fue.id_usuario = '" + req.params.id + "'";
+                return querySql(userQuery, '', req.headers.authorization)
+                    .then(function (rows) {
+                        res.status(200).json({
+                            'status': 'success',
+                            'Empr': rows
+                        });
+                    });
+            }
+
+
+        })
+});
 
 router.post("/empresa", function (req, res) {
-    
+
     var userQuery = "select * from fis_empresa where cnpj = '" + req.body.cnpj + "'";
     return querySql(userQuery, '')
         .then(function (rows) {
@@ -95,6 +114,96 @@ router.post("/empresa", function (req, res) {
         });
 });
 
+router.post("/empresa/nova", function (req, res) {
+    return db.Hash(req.headers.authorization)
+        .then(function (valid) {
+            var userInsert = "INSERT INTO fis_empresa (razao_social, endereco, cnpj, cep, uf, cidade, numero, complemento, bairro, telefone, email, apelido) VALUES ("
+            userInsert += "'" + req.body.razao + "','" + req.body.logradouro + "','" + req.body.CNPJ + "','" + req.body.cep + "','" + req.body.uf + "','" + req.body.cidade + "','" + req.body.numero + "'";
+            userInsert += ",'" + req.body.complemento + "','" + req.body.bairro + "','" + req.body.telefone + "','" + req.body.email + "','" + req.body.apelido + "')";
+            return db.insertSql(userInsert)
+                .then(function (returns) {
+                    res.status(200).json({
+                        'status': 'success'
+                    });
+                });
+        });
+});
+
+router.post("/empresa/minha", function (req, res) {
+    return db.Hash(req.headers.authorization)
+        .then(function (valid) {
+            var userQuery = "select * from fis_empresa where cnpj = '" + req.body.CNPJ + "'";
+            return querySql(userQuery, '')
+                .then(function (rows) {
+                    if (rows.length == 0) {
+                        var userInsert = "INSERT INTO fis_empresa (razao_social, endereco, cnpj, cep, uf, cidade, numero, complemento, bairro, telefone, email, apelido, ie) VALUES ("
+                        userInsert += "'" + req.body.razao + "','" + req.body.logradouro + "','" + req.body.CNPJ + "','" + req.body.cep + "','" + req.body.uf + "','" + req.body.cidade + "','" + req.body.numero + "'";
+                        userInsert += ",'" + req.body.complemento + "','" + req.body.bairro + "','" + req.body.telefone + "','" + req.body.email + "','" + req.body.apelido + "','" + req.body.ie + "')";
+                        return db.insertSql(userInsert)
+                            .then(function (returns) {
+                                var userInsert = "INSERT INTO fis_usuario_empresa (	id_empresa, id_usuario) VALUES ('"
+                                userInsert += returns.insertId + "','" + req.body.id + "')";
+                                return db.insertSql(userInsert)
+                                    .then(function (returns) {
+                                        res.status(200).json({
+                                            'status': 'success'
+                                        });
+                                    });
+                            });
+                    }
+                    else {
+                        var userInsert = "update fis_empresa set "
+                        userInsert += "razao_social = '" + req.body.razao + "',"
+                        userInsert += "endereco = '" + req.body.logradouro + "',"
+                        userInsert += "cnpj = '" + req.body.CNPJ + "',"
+                        userInsert += "cep = '" + req.body.cep + "',"
+                        userInsert += "uf = '" + req.body.uf + "',"
+                        userInsert += "cidade = '" + req.body.cidade + "',"
+                        userInsert += "numero = '" + req.body.numero + "',"
+                        userInsert += "complemento = '" + req.body.complemento + "',"
+                        userInsert += "bairro = '" + req.body.bairro + "',"
+                        userInsert += "telefone = '" + req.body.telefone + "',"
+                        userInsert += "email = '" + req.body.email + "',"
+                        userInsert += "apelido = '" + req.body.apelido + "',"
+                        userInsert += "ie = '" + req.body.ie + "'"
+                        userInsert += "where id_empresa = " + rows[0].id_empresa;
+                        return db.insertSql(userInsert)
+                            .then(function (returns) {
+                                var userInsert = "INSERT INTO fis_usuario_empresa (	id_empresa, id_usuario) VALUES ('"
+                                userInsert += rows[0].id_empresa + "','" + req.body.id + "')";
+                                return db.insertSql(userInsert)
+                                    .then(function (returns) {
+                                        res.status(200).json({ 'status': 'success' });
+                                    });
+                            });
+                    }
+                });
+        });
+});
+
+router.patch("/empresa/minha", function (req, res) {
+
+    var userInsert = "update fis_empresa set "
+    userInsert += "razao_social = '" + req.body.razao + "',"
+    userInsert += "endereco = '" + req.body.logradouro + "',"
+    userInsert += "cnpj = '" + req.body.CNPJ + "',"
+    userInsert += "cep = '" + req.body.cep + "',"
+    userInsert += "uf = '" + req.body.uf + "',"
+    userInsert += "cidade = '" + req.body.cidade + "',"
+    userInsert += "numero = '" + req.body.numero + "',"
+    userInsert += "complemento = '" + req.body.complemento + "',"
+    userInsert += "bairro = '" + req.body.bairro + "',"
+    userInsert += "telefone = '" + req.body.telefone + "',"
+    userInsert += "email = '" + req.body.email + "',"
+    userInsert += "apelido = '" + req.body.apelido + "',"
+    userInsert += "ie = '" + req.body.ie + "'"
+    userInsert += "where id_empresa = " + req.body.id_empresa;
+    return db.insertSql(userInsert)
+        .then(function (returns) {
+            res.status(200).json({ 'status': 'success' });
+
+        });
+});
 
 router.patch("/empresa", function (req, res) {
     return db.Hash(req.headers.authorization)
@@ -127,9 +236,5 @@ router.delete("/empresa", function (req, res) {
             }
         });
 });
-
-
-
-
 
 module.exports = router;
